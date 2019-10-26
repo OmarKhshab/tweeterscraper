648c4406400e56ff35d7d3b57b16e10edac2c8b0
OmarKhshab <45400350+OmarKhshab@users.noreply.github.com>
2019-10-26 20:30:07 +0200

    Delete tweet.py

--- tweet.py
+++
@@ -1,8 +0,0 @@
-class Tweet:
-  def __init__(self, tweetId,tweetText, username,userId,screenName,timestamp):
-    self.tweetId = tweetId
-    self.tweetText = tweetText
-    self.username = username
-    self.userId = userId 
-    self.screenName = screenName
-    self.timestamp = timestamp
